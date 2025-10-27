9d4aa56553404ecd835f7588e8053f34f98cad5f
Joao Gabriel <joaogabriel2007hp@gmail.com>
2025-10-27 18:06:38 -0300

    Atualização Animação Scroll

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function initScrollTo() {
     item.preventDefault();
     const href = this.getAttribute("href");
     const section = document.querySelector(href);
-    console.log(section);
+    // console.log(section);
     section.scrollIntoView({
       behavior: "smooth",
       block: "start",
@@ -19,3 +19,22 @@ function initScrollTo() {
 }
 
 initScrollTo();
+
+function initSmoothScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const window6Size = window.outerHeight * 0.6;
+    function whenScroll() {
+      sections.forEach((item) => {
+        const topOf = item.getBoundingClientRect().top < 0 + window6Size;
+        if (topOf) {
+          item.classList.add("ativo");
+        }
+      });
+    }
+    sections[0].classList.add("ativo");
+    window.addEventListener("scroll", whenScroll);
+  }
+}
+
+initSmoothScroll();
